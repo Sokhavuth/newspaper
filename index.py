f0795e63f4b9d9ba1ecd75c2121b641d65daede4
Sokhavuth TIN <vuthdevelop@gmail.com>
2021-01-14 12:58:58 +0700

    Make it better

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 #index.py
-from flask import render_template, session
+from flask import render_template
 from flask_classful import FlaskView, route
 from models.postdb import Postdb
   
@@ -7,13 +7,6 @@ class Index(FlaskView):
   
   @route('/')
   def index(self):
-    session['pageToken'] = []
-    self.postdb = Postdb()
-    vdict = self.postdb.get_post()
-    return render_template('index.html', data=vdict)
-
-  @route('/load/')
-  def load_post(self):
     self.postdb = Postdb()
     vdict = self.postdb.get_post()
     return render_template('index.html', data=vdict)
--- models/postdb.py
+++ models/postdb.py
@@ -1,6 +1,6 @@
 #models/post.py
-import config, copy, requests, lib, json, datetime
-from flask import render_template, session
+import config, copy, lib
+from flask import render_template
 from flask_classful import FlaskView, route
 
 class Postdb():
@@ -9,38 +9,6 @@ class Postdb():
     vdict = copy.deepcopy(config.vdict)
     
     self.vlib = lib.Lib()
-    '''
-    query = {'key':vdict['api-key'], 'maxResults':26}
-    if session['pageToken']:
-      query['pageToken'] = session['pageToken'][-1]
-
-    def getResponse(response, *args, **kwargs):
-      nonlocal vdict, query
-      
-      json = response.json()
-      session['pageToken'].append(json['nextPageToken'])
-      print(session['pageToken'])
-      posts = json['items']
-      list = []
-      
-      for post in posts:
-        id = post['id']
-        title = post['title']
-        content = post['content']
-        video = content.find('__video-id__')
-        date = post['published']
-        date = datetime.datetime.fromisoformat(date)
-        date = datetime.datetime.strftime(date, "%d/%m/%Y")
-        url = post['url']
-        author = post['author']['displayName']
-
-        list.append((id, title, content, date, url, author, video))
-
-      vdict['posts'] = list
-      vdict['thumbs'] = self.vlib.get_thumbs(list, 2)
-      '''
     vdict['date'] = self.vlib.set_date()
-
-    #requests.get('https://www.googleapis.com/blogger/v3/blogs/'+vdict['blog-id']+'/posts', params=query, hooks={'response': getResponse})
     
     return vdict
