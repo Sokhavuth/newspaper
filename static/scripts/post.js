1379e9d275bb64f48950872a43f1bc2365fda013
Sokhavuth TIN <vuthdevelop@gmail.com>
2021-01-15 19:52:20 +0700

    Make it better

--- static/scripts/post.js
+++ static/scripts/post.js
@@ -8,6 +8,89 @@ class Post{
     var khDate = d+'/'+m+'/'+y;
     return khDate;
   }
+
+  setPostVid(eleId){
+    var playlist = document.createElement( 'div' );
+    var description = document.createElement( 'div');
+    var post = document.getElementById(eleId);
+    var kbplayer = document.getElementById("KBPlayer");
+  
+    var str = post.getElementsByClassName("__video-id__")[0].getAttribute("data-id");
+    playlist.innerHTML = post.getElementsByClassName("__playlist__")[0].getAttribute("data-pl");
+    description.innerHTML = post.getElementsByClassName("__description__")[0].innerHTML;
+  
+    kbplayer.parentElement.insertBefore(description, kbplayer.nextSibling);
+    var startIndex = str.indexOf('{');
+    var endIndex = str.indexOf('}');
+    var vidId = str.slice(startIndex+1,endIndex);
+
+    if(str.indexOf('googledrive') != -1){
+      var iframeSrc = 'https://docs.google.com/file/d/'+vidId+'/preview';
+
+    }
+  
+    else if(str.indexOf('youtube') != -1){
+      var iframeSrc = '//www.youtube.com/embed/'+vidId;
+
+    }
+
+    else if(str.indexOf('facebookvid') != -1){
+      var iframeSrc = 'https://www.facebook.com/watch/?v='+vidId;
+    
+    }
+
+    else if(str.indexOf('dailymotion') != -1){
+      var iframeSrc = '//www.dailymotion.com/embed/video/'+vidId+'?logo=0&info=0';
+
+    }
+
+    else if(str.indexOf('vimeo') != -1){
+      var iframeSrc = '//player.vimeo.com/video/'+vidId;
+
+    }
+
+    else if(str.indexOf('ok') != -1){
+      var iframeSrc = '//ok.ru/videoembed/'+vidId;
+
+    }
+
+
+    if(str.indexOf('facebookvid') != -1){
+      var postContent = '<p width="100%" id="fb-outer">';
+      postContent += '<div class="fb-video" data-width="auto" data-autoplay="false" data-allowfullscreen="true" data-href="'+iframeSrc+'"></div>';
+      postContent += '</p>'; 
+    }
+    
+    else{
+      var postContent = '<iframe id="player" src="'+iframeSrc+'" width="100%" heigh="150" frameborder="0" allowfullscreen webkitallowfullscreen mozallowfullscreen SCROLLING=NO ></iframe>';
+    }
+   
+    post.innerHTML = postContent;
+
+    var Player = post.getElementsByTagName('iframe');
+   
+    if(str.indexOf('facebookvids') != -1){
+      var vidWidth = post.getElementsByTagName('p');
+      var Player = post.getElementsByTagName('div');
+      Player[0].setAttribute("data-width", vidWidth[0].clientWidth);
+    }
+    else{
+      var vidWidth = Player[0].clientWidth;
+      Player[0].height = vidWidth / 16 * 9;
+    } 
+   /*
+    str = playlist.innerHTML;
+    if(str.indexOf('pl') != -1){
+      var startIndex = str.indexOf('[');
+      var endIndex = str.indexOf(']');
+      this.pl = str.slice(startIndex+1,endIndex);
+     
+      var relatedPostUrl = "https://www.googleapis.com/blogger/v3/blogs/"+KNclass.blogId+"/posts?maxResults=300&labels="+this.pl+"&callback=POST.getRelatedPost&key="+KNclass.apiKey;
+      $.getScript(relatedPostUrl);
+    }   
+    */
+  }
+    
 }//end class
 
 const post = new Post();
